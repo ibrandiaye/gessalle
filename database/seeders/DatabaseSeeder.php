02bdb8bad3206c97f3d051f56107252252e62365
ibrandiaye <ibra789ndiaye@gmail.com>
2025-07-14 15:24:23 +0000

    deploy 2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +16,18 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+       /* User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);*/
+        DB::table('users')->insert([
+            'name' => 'Super Admin',
+            'email' => 'superadmin@gmail.com',
+            'password' => bcrypt('P@sser123'),
+            'created_at' => now(),
+            'updated_at' => now(),
+            'role' =>'superadmin',
+            'salle_id' => null
         ]);
     }
 }
