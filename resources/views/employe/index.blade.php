fcab51edb1931c6058edd4475991bb5fbe9f0ebf
cheikh diop <cheikh.diop@conforts.net>
2025-10-30 11:58:30 +0000

    my commit'

--- resources/views/employe/index.blade.php
+++ resources/views/employe/index.blade.php
@@ -21,16 +21,7 @@
     <div class="clearfix"></div>
 </div>
 
-@if ($message = Session::get('success'))
-<div class="alert alert-success">
-    <p>{{ $message }}</p>
-</div>
-@endif
-@if ($message = Session::get('error'))
-<div class="alert alert-danger">
-    <p>{{ $message }}</p>
-</div>
-@endif
+
 @if ($errors->has('licence'))
 <div class="alert alert-danger">
     {!! $errors->first('licence') !!} . <a href="{{route('indexClient')}}"><u>Merci de renouveler votre licence pour pouvoir continuer</u></a>
