6ca0cb952619d1da257d56eb3a5580aadf5ccf95
cheikh diop <cheikh.diop@conforts.net>
2025-10-30 12:15:00 +0000

    my commit'

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -21,16 +21,6 @@
                     <div class="clearfix"></div>
                 </div>
 
-    @if ($message = Session::get('success'))
-        <div class="alert alert-success">
-            <p>{{ $message }}</p>
-        </div>
-    @endif
-    @if ($message = Session::get('error'))
-        <div class="alert alert-danger">
-            <p>{{ $message }}</p>
-        </div>
-    @endif
 
 
     @if ($errors->has('licence'))
