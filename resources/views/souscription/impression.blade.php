7a629162072c1d9611eef34a9790160476facf60
cheikh diop <cheikh.diop@conforts.net>
2025-10-30 12:29:20 +0000

    my commit'

--- resources/views/souscription/impression.blade.php
+++ resources/views/souscription/impression.blade.php
@@ -3,9 +3,9 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  @php
+  {{-- @php
   $salle =  auth::user()->salle;
-  @endphp
+  @endphp --}}
   <title>{{$salle->nom}}</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
