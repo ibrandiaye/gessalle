5a9f2fde89292fe9b6ce18a72cf13e977720b110
cheikh diop <cheikh.diop@conforts.net>
2025-10-30 12:33:02 +0000

    my commit'

--- resources/views/souscription/impression.blade.php
+++ resources/views/souscription/impression.blade.php
@@ -1,127 +1,146 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  {{-- @php
-  $salle =  auth::user()->salle;
-  @endphp --}}
-  <title>{{$salle->nom}}</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- Bootstrap 4 -->
+    <meta charset="utf-8">
+    <title>{{ $salle->nom }}</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600&display=swap" rel="stylesheet">
 
-  <link href="{{asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{asset('css/icons.css') }}" rel=   "stylesheet" type="text/css">
-    <link href="{{asset('css/style.css') }}" rel="stylesheet" type="text/css">
-  <!-- Google Font: Source Sans Pro -->
-  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Source Sans Pro', sans-serif;
+            background-color: #f8f9fa;
+            font-size: 18px;
+            padding: 20px;
+        }
+
+        .ticket-wrapper {
+            width: 110mm;
+            margin: 0 auto;
+            background: #fff;
+            border-radius: 10px;
+            padding: 20px;
+            box-shadow: 0 2px 6px rgba(0,0,0,0.1);
+        }
+
+        .ticket-header {
+            text-align: center;
+            border-bottom: 2px dashed #999;
+            padding-bottom: 10px;
+            margin-bottom: 15px;
+        }
+
+        .ticket-header img {
+            height: 60px;
+            margin-bottom: 10px;
+        }
+
+        .ticket-title {
+            font-weight: 700;
+            font-size: 22px;
+            margin: 0;
+        }
+
+        .ticket-info table {
+            width: 100%;
+            font-size: 17px;
+        }
+
+        .ticket-info td {
+            padding: 3px 0;
+        }
+
+        .ticket-summary {
+            border-top: 2px dashed #999;
+            border-bottom: 2px dashed #999;
+            padding: 10px 0;
+            margin: 15px 0;
+            text-align: center;
+            font-weight: 600;
+        }
+
+        .qrcode {
+            text-align: center;
+            margin-top: 15px;
+        }
+
+        .btn-back {
+            display: inline-block;
+            margin-bottom: 20px;
+        }
+
+        @media print {
+            body {
+                background: #fff;
+                padding: 0;
+            }
+            .btn-back {
+                display: none;
+            }
+        }
+    </style>
 </head>
-<style>
-
-  th,td {
-   /* border: solid 1px;*/
-   /* text-align: center;*/
-    font-weight: bold;
-  }
-  table {
-
-                border-collapse: collapse;
-                font-size: 23px;
-            }
-            body
-            {
-                font-size: 23px;
-            }
-            .wrapper
-            {
-                width: 110mm; /* Largeur pour l'impression */
-            }
-            .invoice
-            {
-                width: 110mm; /* Largeur pour l'impression */
-            }
-</style>
 <body>
-<div class="wrapper">
-  <!-- Main content -->
-  <section class="invoice">
-    <div class="row">
-        <div class="col-12">
 
-
-
-          <!-- Main content -->
-          <div class="invoice p-3 mb-3">
-            <div class="row">
-                <div class="col-12">
-                  <h4>
-                 {{--    <img class="img float-right" src="{{ asset('assets/img/logo.png') }}" style="width: 20%;"> --}}
-
-
-                  </h4>
-                </div>
-                <!-- /.col -->
-              </div>
-              <!-- info row -->
-              <center>
-                @if($salle->logo)
-                    <img class="img " src="{{ asset('logo/'.$salle->logo) }}" style="height: 50px;"><br>
-
-                @endif
-              </center>
-              <center>{{ $salle->nom }}</center><br>
-              <center>Adresse{{ $salle->adresse }}</center><br>
-             <center> ------------------------------</center>
-
-             <table style="width: 100%;border: none;">
-                <tr>
-                    <td><b>N° Facture </b></td>
-                    <td class="float-right"><b>{{ $souscription->id }}</b></td>
-                </tr>
-                <tr>
-                    <td><b> Client</b></td>
-                    <td class="float-right"><b><strong>
-                    {{ $souscription->prenom }} {{ $souscription->nom }} </strong></b></td>
-                </tr>
-                <tr>
-                    <td><b> TEL</b></td>
-                    <td class="float-right"><b><strong>
-                    {{ $souscription->tel }}  </strong></b></td>
-                </tr>
-                <tr>
-                    <td><b>  Date</b></td>
-                    <td class="float-right"><b> {{  Carbon\Carbon::parse( $souscription->created_at)->format('d/m/Y H:i') }}</b></td>
-                </tr>
-                <tr>
-                    <td><b>  INFOLINE</b></td>
-                    <td class="float-right"><b> {{ $salle->adresse }}</b></td>
-                </tr>
-
-             </table>
-
-
-
-            <!-- /.row -->
-            <center> ------------------------------</center>
-            <strong>Montant : {{$souscription->prix}} CFA</strong><br>
-            <strong>Debut : {{Carbon\Carbon::parse( $souscription->date_debut)->format('d/m/Y') }}</strong><br>
-            <strong>Fin : {{Carbon\Carbon::parse( $souscription->date_fin)->format('d/m/Y') }}</strong><br>
-            <center> ------------------------------</center>
-             <center>{{ $qrcode }}</center>
-
-          </div>
-          <!-- /.invoice -->
-        </div><!-- /.col -->
-      </div><!-- /.row -->
-      <br><br>
-  </section>
-  <!-- /.content -->
+<div class="text-center">
+    <a href="{{ url()->previous() }}" class="btn btn-secondary btn-sm btn-back">
+        ⬅️ Retour
+    </a>
 </div>
-<!-- ./wrapper -->
 
-<script type="text/javascript">
-  window.addEventListener("load", window.print());
+<div class="ticket-wrapper">
+    <div class="ticket-header">
+        @if($salle->logo)
+            <img src="{{ asset('logo/'.$salle->logo) }}" alt="Logo {{ $salle->nom }}">
+        @endif
+        <h1 class="ticket-title">{{ $salle->nom }}</h1>
+        <p class="text-muted mb-0">{{ $salle->adresse }}</p>
+    </div>
+
+    <div class="ticket-info">
+        <table>
+            <tr>
+                <td><strong>N° Facture :</strong></td>
+                <td class="text-end">{{ $souscription->id }}</td>
+            </tr>
+            <tr>
+                <td><strong>Client :</strong></td>
+                <td class="text-end">{{ $souscription->prenom }} {{ $souscription->nom }}</td>
+            </tr>
+            <tr>
+                <td><strong>Téléphone :</strong></td>
+                <td class="text-end">{{ $souscription->tel }}</td>
+            </tr>
+            <tr>
+                <td><strong>Date :</strong></td>
+                <td class="text-end">{{ Carbon\Carbon::parse($souscription->created_at)->format('d/m/Y H:i') }}</td>
+            </tr>
+            <tr>
+                <td><strong>Infoline :</strong></td>
+                <td class="text-end">{{ $salle->adresse }}</td>
+            </tr>
+        </table>
+    </div>
+
+    <div class="ticket-summary">
+        <div>Montant : <strong>{{ $souscription->prix }} CFA</strong></div>
+        <div>Début : <strong>{{ Carbon\Carbon::parse($souscription->date_debut)->format('d/m/Y') }}</strong></div>
+        <div>Fin : <strong>{{ Carbon\Carbon::parse($souscription->date_fin)->format('d/m/Y') }}</strong></div>
+    </div>
+
+    <div class="qrcode">
+        {!! $qrcode !!}
+    </div>
+</div>
+
+<script>
+    window.addEventListener('load', () => {
+        // Supprime la ligne suivante si tu veux éviter l’impression automatique
+        // window.print();
+    });
 </script>
+
 </body>
 </html>
